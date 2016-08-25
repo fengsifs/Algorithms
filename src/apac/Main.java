1399cf06e1cd6a48a05262454afd39228cc674e0
fengsi <feiyoshang@gmail.com>
2016-08-25 19:13:18 +0800

    Country Leader

--- src/apac/Main.java
+++ src/apac/Main.java
@@ -9,30 +9,32 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            BufferedReader bfr = new BufferedReader(new FileReader(new File("resource/B-large-practice.in")));
-            BufferedWriter bfw = new BufferedWriter(new FileWriter(new File("resource/BL.out")));
-            String line = bfr.readLine();
-            int lines = Integer.parseInt(line);
-            int k = 1;
-            while (k <= lines) {
-                line = bfr.readLine();
-                String[] s = line.split(" ");
-                int n = Integer.parseInt(s[0]);
-                int l = Integer.parseInt(s[1]);
-                int[][] nums = new int[4][n];
-                int a = 0;
-                while (a < 4) {
-                    line = bfr.readLine();
-                    s = line.split(" ");
-                    for (int i = 0; i < s.length; i++)
-                        nums[a][i] = Integer.parseInt(s[i]);
-                    a++;
+//            BufferedReader bfr = new BufferedReader(new FileReader(new File("resource/B-large-practice.in")));
+            Scanner scanner = new Scanner(new FileInputStream(new File("resource/A-large-practice.in")));
+            BufferedWriter bfw = new BufferedWriter(new FileWriter(new File("resource/AL.out")));
+            int k = 0;
+            int t = scanner.nextInt();
+            while (k++ < t) {
+                int n = scanner.nextInt();
+                Map<Integer, PriorityQueue<String>> map = new HashMap<>();
+                int max = 0;
+                scanner.nextLine();
+                while (n-- > 0) {
+                    String s = scanner.nextLine();
+                    int c = CountString.count(s);
+                    if (c > max) {
+                        max = c;
+                        PriorityQueue<String> pq = new PriorityQueue<>();
+                        pq.offer(s);
+                        map.put(c, pq);
+                    } else if (c == max) {
+                        map.get(c).offer(s);
+                    }
                 }
-                bfw.write("Case #" + k + ": " + new RobotRockBand().count(nums, l));
+                bfw.write("Case #" + k + ": " + map.get(max).peek());
                 bfw.newLine();
-                k++;
             }
-            bfr.close();
+            scanner.close();
             bfw.flush();
             bfw.close();
         } catch (IOException e) {
