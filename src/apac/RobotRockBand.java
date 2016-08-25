81c59765e2721c0c020bb57ea9eeadb8e1c3572f
fengsi <feiyoshang@gmail.com>
2016-08-25 18:27:25 +0800

    Robot Rock Band

--- src/apac/RobotRockBand.java
+++ src/apac/RobotRockBand.java
@@ -1,19 +1,27 @@
 package apac;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by FengSi on 2016/08/25 at 12:45.
  */
 public class RobotRockBand {
-    public int count (int[][] nums, int k) {
-        int res = 0;
-        for (int a = 0; a < nums[0].length; a++) {
-            for (int b = 0; b < nums[0].length; b++) {
-                for (int c = 0; c < nums[0].length; c++) {
-                    for (int d = 0; d < nums[0].length; d++) {
-                        if ((nums[0][a]^nums[1][b]^nums[2][c]^nums[3][d]) == k) {
-                            res++;
-                        }
-                    }
+    public long count (int[][] nums, int k) {
+        long res = 0;
+        Map<Integer, Long> map = new HashMap<>();
+        for (int i : nums[0]) {
+            for (int j : nums[1]) {
+                map.put(i ^ j, (map.containsKey(i ^ j) ? map.get(i ^ j) + 1 : (long) 1));
+            }
+        }
+        for (int i : nums[2]) {
+            for (int j : nums[3]) {
+                int x = i ^ j ^ k;
+                if (map.containsKey(x)) {
+                    res += map.get(x);
                 }
             }
         }
