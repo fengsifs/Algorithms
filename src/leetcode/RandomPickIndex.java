78a3b6778737f32caf3883b266a9d5f365a20d62
feiyoshang@gmail.com <1989hereiam>
2016-09-11 15:49:08 +0800

    LeetCode Weekly Contest 4

---
+++ src/leetcode/RandomPickIndex.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by FengSi on 2016/09/11 at 15:34.
+ */
+public class RandomPickIndex {
+    Map<Integer, List<Integer>> map = new HashMap<>();
+
+    public RandomPickIndex(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            List<Integer> list = map.containsKey(nums[i]) ? map.get(nums[i]) : new ArrayList<>();
+            list.add(i);
+            map.put(nums[i], list);
+        }
+    }
+
+    public int pick(int target) {
+        List<Integer> list = map.get(target);
+        return list.get((int)(Math.random() * list.size()));
+    }
+}
+
+/*
+398. Random Pick Index
+Given an array of integers with possible duplicates, randomly output the index of a given target number.
+You can assume that the given target number must exist in the array.
+
+Note:
+The array size can be very large. Solution that uses too much extra space will not pass the judge.
+
+Example:
+
+int[] nums = new int[] {1,2,3,3,3};
+Solution solution = new Solution(nums);
+
+// pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+solution.pick(3);
+
+// pick(1) should return 0. Since in the array only nums[0] is equal to 1.
+solution.pick(1);
+ */
