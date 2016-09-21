7b9e0b19caf34f3ed55c4cd46ba7f8fd1460bdce
fengsi <feiyoshang@gmail.com>
2016-09-21 12:43:14 +0800

    Java 8 Stream

--- src/java8/StreamTest.java
+++ src/java8/StreamTest.java
@@ -1,10 +1,8 @@
 package java8;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.joining;
@@ -29,18 +27,21 @@ public class StreamTest {
                 new Transaction(alan, 2012, 950)
         );
 
+        // Find all transactions in the year 2011 and sort them by value (small to high).
         List<Transaction> list = transactions.stream()
                 .filter(transaction -> transaction.getYear() == 2011)
                 .sorted(comparing(Transaction::getValue))
                 .collect(toList());
         list.forEach(System.out::println);
 
+        // What are all the unique cities where the traders work?
         List<String> cities = transactions.stream()
                 .map(transaction -> transaction.getTrader().getCity())
                 .distinct()
                 .collect(toList());
         cities.forEach(System.out::println);
 
+        // Find all traders from Cambridge and sort them by name
         List<Trader> traders = transactions.stream()
                 .map(Transaction::getTrader)
                 .filter(trader -> trader.getCity().equals("Cambridge"))
@@ -49,6 +50,7 @@ public class StreamTest {
                 .collect(toList());
         traders.forEach(System.out::println);
 
+        // Return a string of all traders’ names sorted alphabetically.
         String name = transactions.stream()
                 .map(transaction -> transaction.getTrader().getName())
                 .distinct()
@@ -57,21 +59,25 @@ public class StreamTest {
 //                .reduce("", (a, b) -> a + b);
         System.out.println(name);
 
+        // Are any traders based in Milan?
         boolean milan = transactions.stream()
                 .anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan"));
         System.out.println(milan);
 
+        // Print all transactions’ values from the traders living in Cambridge.
         List<Transaction> transactions1 = transactions.stream()
                 .filter(transaction -> transaction.getTrader().getCity().equals("Cambridge"))
                 .collect(toList());
         transactions1.forEach(System.out::println);
 
+        // What’s the highest value of all the transactions?
         Optional<Integer> max = transactions.stream()
                 .map(Transaction::getValue)
                 .reduce(Integer::max);
         int m = max.isPresent() ? max.get() : 0;
         System.out.println(m);
 
+        // Find the transaction with the smallest value.
         Optional<Transaction> transaction = transactions.stream()
                 .sorted(comparing(Transaction::getValue))
                 .findFirst();
