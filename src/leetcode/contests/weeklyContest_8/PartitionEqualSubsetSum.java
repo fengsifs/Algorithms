6e578aa288cdc62fb723170263568a55616e121d
fengsi <feiyoshang@gmail.com>
2016-10-09 15:58:10 +0800

    LeetCode Weekly Contest 8

---
+++ src/leetcode/contests/weeklyContest_8/PartitionEqualSubsetSum.java
@@ -0,0 +1,31 @@
+package leetcode.contests.weeklyContest_8;
+
+/**
+ * Created by FengSi on 2016/10/09 at 10:53.
+ */
+public class PartitionEqualSubsetSum {
+}
+
+/*
+416. Partition Equal Subset Sum My SubmissionsBack To Contest
+Given a non-empty array containing only positive integers, find if the
+array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+Note:
+Both the array size and each of the array element will not exceed 100.
+
+Example 1:
+
+Input: [1, 5, 11, 5]
+
+Output: true
+
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: [1, 2, 3, 5]
+
+Output: false
+
+Explanation: The array cannot be partitioned into equal sum subsets.
+ */
