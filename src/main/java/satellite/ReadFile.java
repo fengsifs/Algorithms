60b8af92e94b398da9454477292a1b6dacbb11d8
fengsi <feiyoshang@gmail.com>
2016-11-11 19:30:59 +0800

    satellite

--- src/main/java/satellite/ReadFile.java
+++ src/main/java/satellite/ReadFile.java
@@ -1,5 +1,8 @@
 package satellite;
 
+import Jama.Matrix;
+import Jama.SingularValueDecomposition;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +14,15 @@ public class ReadFile {
     public static void main(String[] args) {
         String path = "D:\\Data\\HJ010C\\satellite";
         double[][] a = points(path);
-        for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(a[i][j] + " ");
-            }
-            System.out.println();
-        }
+        Matrix m = new Matrix(a);
+        SingularValueDecomposition svd = m.svd();
+        Matrix u = svd.getU();
+        System.out.println("u: " + u.getColumnDimension() + " x " + u.getRowDimension());
+        Matrix s = svd.getS();
+        System.out.println("u: " + s.getColumnDimension() + " x " + s.getRowDimension());
+        Matrix v = svd.getV();
+        System.out.println("v: " + v.getColumnDimension() + " x " + v.getRowDimension());
+
     }
 
     public static List<String> timeList(String path) {
@@ -26,6 +32,7 @@ public class ReadFile {
             String line = null;
             while ((line = bfr.readLine()) != null)
                 time.add(line.split(",")[0]);
+            bfr.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -38,6 +45,7 @@ public class ReadFile {
         try {
             BufferedReader bfr;
             int k = 0;
+            int size = Integer.MAX_VALUE;
             for (File f : fs) {
                 bfr = new BufferedReader(new FileReader(f));
                 List<Double> temp = new ArrayList<>();
@@ -46,10 +54,18 @@ public class ReadFile {
                     temp.add(Double.parseDouble(s.split(",")[1]));
                 points[k] = new double[temp.size()];
                 int i = 0;
+                if (temp.size() < size)
+                    size = temp.size();
                 for (Double aDouble : temp)
                     points[k][i++] = aDouble;
                 k++;
+                bfr.close();
             }
+            double[][] tempP = new double[fs.length][size];
+            for (int i = 0; i < tempP.length; i++)
+                for (int j = 0; j < tempP[0].length; j++)
+                    tempP[i][j] = points[i][j];
+            points = tempP;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -63,24 +79,28 @@ public class ReadFile {
 
     private static void writeFile(String prefix) {
         List<String> time = new ArrayList<>();
-        List<Double> points = new ArrayList<>();
+        List<String> points = new ArrayList<>();
         try {
             BufferedReader bfr;
             File[] fs = fs(prefix);
+            int count = 0;
             for (File f : fs) {
                 bfr = new BufferedReader(new FileReader(f));
                 String line = null;
                 while ((line = bfr.readLine()) != null) {
                     String[] temp = line.split(",");
                     time.add(temp[0]);
-                    points.add(Double.parseDouble(temp[1]));
+                    points.add(temp[1]);
                 }
+                bfr.close();
             }
+            System.out.println(time.get(time.size()-1));
             BufferedWriter bfw = new BufferedWriter(new FileWriter(new File(prefix + ".txt")));
             for (int i = 0; i < time.size(); i++) {
                 bfw.write(time.get(i) + "," + points.get(i));
                 bfw.newLine();
             }
+            bfw.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
